1d60d44c970c882041e3de113f4939ea1eba581c
FunCheney <fc_2012@163.com>
2019-05-03 12:09:46 +0800

    前序遍历二分搜索树

--- src/main/java/com/fchen/datastructure/tree/binarysearchtree/BST.java
+++ src/main/java/com/fchen/datastructure/tree/binarysearchtree/BST.java
@@ -32,31 +32,74 @@ public class BST<E extends Comparable<E>> {
      * @param e
      */
     public void add(E e){
-        if(root == null){
-            root = new Node<>(e);
+        //从根节点开始添加元素
+        root = add(root, e);
+    }
+
+    /**
+     * 返回插入新节点后，二分搜索树的根
+     * @param node
+     * @param e
+     * @return
+     */
+    private Node add(Node node,E e){
+        if(node == null){
+            size++;
+            return new Node(e);
+        }
+        if(e.compareTo((E)node.e) < 0){
+            node.left = add(node.left,e);
+        }else if(e.compareTo((E)node.e) > 0){
+            node.right = add(node.right,e);
+        }
+        return node;
+    }
+
+    /**
+     * 查看二分搜索树中是否包含元素e
+     * @param e
+     * @return
+     */
+    public boolean contains(E e){
+        return contains(root, e);
+    }
+
+    /**
+     * 以node为根的二分搜索书中是否包含元素e
+     * @param node
+     * @param e
+     * @return
+     */
+    private boolean contains(Node node, E e){
+        if(node == null){
+            return false;
+        }
+        if(e.compareTo((E)node.e) < 0){
+            return contains(node.left, e);
+        }else if(e.compareTo((E)node.e) > 0){
+            return contains(node.right,e);
         }else{
-            //从根节点开始添加元素
-            add(root, e);
+            return true;
         }
     }
 
-    private void add(Node node,E e){
-        if(e.equals(node.e)){
+    /**
+     * 前序遍历二叉树
+     */
+    public void preOrder(){
+
+    }
+
+    /**
+     * 前序遍历以node为根的二分搜索树
+     * @param node
+     */
+    private void preOrder(Node node){
+        if(node == null){
             return;
-        }else if(e.compareTo((E)node.e) < 0 && node.left == null ){
-            node.left = new Node(e);
-            size++;
-            return;
-        }else if(e.compareTo((E)node.e) > 0 && node.right == null){
-            node.right = new Node(e);
-            size++;
-            return;
-        }else{
-            if(e.compareTo((E)node.e) < 0){
-                add(node.left,e);
-            }else{
-                add(node.right,e);
-            }
         }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
     }
 }
