1dc410ac0ec3eb376ff052741a18225a71f19aa4
FunCheney <fc_2012@163.com>
2019-05-12 14:50:28 +0800

    平和搜索树

---
+++ src/main/java/com/fchen/datastructure/tree/avltree/AVLTree.java
@@ -0,0 +1,10 @@
+package com.fchen.datastructure.tree.avltree;
+
+/**
+ * @Classname AVLTree
+ * @Description 平衡搜索树
+ * @Date 2019/5/12 14:49
+ * @Author by Chen
+ */
+public class AVLTree {
+}
---
+++ src/main/java/com/fchen/datastructure/union/UnionFind6.java
@@ -0,0 +1,69 @@
+package com.fchen.datastructure.union;
+
+/**
+ * @Classname UnionFind3
+ * @Description 并查集实现方式六
+ * @Date 2019/5/11 21:55
+ * @Author by Chen
+ */
+public class UnionFind6 implements UF{
+    private int[] parent;
+
+    /**
+     * rank[i] 表示以i为根的元素集合所表示树的层数
+     */
+    private int[] rank;
+
+    public UnionFind6(int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for(int i = 0; i < parent.length; i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if(pRoot == qRoot){
+            return;
+        }
+        //根据两个元素所在树的元素rank不同判断合并方向
+        //将rank低的集合合并到rank高的集合上
+        if(rank[pRoot] < rank[qRoot]){
+            parent[pRoot] = qRoot;
+        }else if(rank[qRoot] < rank[pRoot]){
+            parent[qRoot] = pRoot;
+        }else{
+            parent[qRoot] = pRoot;
+            rank[pRoot] += 1;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+
+    /**
+     * 查找过程，查找元素p对应的集合编号
+     * @param p
+     * @return
+     */
+    private int find(int p){
+        if(p < 0 || p >= parent.length){
+            throw new IllegalArgumentException("p is out of bound");
+        }
+        if (p != parent[p]){
+            parent[p] = find(parent[p]);
+        }
+        return parent[p];
+    }
+}
