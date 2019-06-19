fe14d26ac7bc4ff857709ce837f1e396e5ecacf1
Fchen <fc_2012@163.com>
2019-06-19 21:32:03 +0800

    leetcode

--- src/main/java/com/fchen/datastructure/linkedlist/leetcode/DesignLinkedList.java
+++ src/main/java/com/fchen/datastructure/linkedlist/leetcode/DesignLinkedList.java
@@ -21,63 +21,66 @@ public class DesignLinkedList {
             this(val,null);
         }
         public Node(){
-
         }
     }
     private Node head;
 
     private int size;
-    /** Initialize your data structure here. */
+
+    /** InitializeMyLinkedList your data structure here. */
     public DesignLinkedList() {
         head = new Node(0);
         size = 0;
     }
 
-    public int getSize(){
-        return size;
-    }
+
     /** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
     public int get(int index) {
         if(index < 0 || index >= size){
-            throw new IllegalArgumentException("index illegal");
+            return -1;
         }
-        Node curNode = head.next;
+        Node cur = head.next;
         for(int i = 0; i < index; i++){
-            head = head.next;
+            cur = cur.next;
         }
-        return head.val;
+        return cur.val;
     }
 
     /** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
     public void addAtHead(int val) {
-        addAtIndex(0,val);
+       addAtIndex(0,val);
     }
 
     /** Append a node of value val to the last element of the linked list. */
     public void addAtTail(int val) {
-        addAtIndex(getSize(),val);
+        addAtIndex(size,val);
     }
 
     /** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
     public void addAtIndex(int index, int val) {
         if(index < 0 || index > size){
-            throw new IllegalArgumentException("index illegal");
+            return;
         }
-        Node insertNode = new Node(val,null);
-        Node prev = null;
-        for (int i = 0; i < index; i++) {
-            prev = head.next;
-            head = prev.next;
+        Node prev = head;
+        for(int i = 0; i < index; i++){
+            prev = prev.next;
         }
-        prev.next = insertNode;
-        insertNode.next = head.next;
+        prev.next = new Node(val,prev.next);
+        size++;
     }
 
     /** Delete the index-th node in the linked list, if the index is valid. */
     public void deleteAtIndex(int index) {
-        if(index < 0 || index >= size){
-            throw new IllegalArgumentException("index illegal");
+        if(index < 0 || index > size){
+            return;
         }
-
+        Node prev = head;
+        for(int i = 0; i < index; i++){
+            prev = prev.next;
+        }
+        Node delNode = prev.next;
+        prev.next = delNode.next;
+        delNode.next = null;
+        size--;
     }
 }
