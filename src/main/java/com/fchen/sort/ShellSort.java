4938002ca6a86b7a3eb119e30a2db80be6901c4f
FunCheney <fc_2012@163.com>
2019-07-08 23:27:01 +0800

    xier排序

---
+++ src/main/java/com/fchen/sort/ShellSort.java
@@ -0,0 +1,26 @@
+package com.fchen.sort;
+
+/**
+ * @Classname ShellSort
+ * @Description 希尔排序
+ * @Date 2019/7/8 22:57
+ * @Author by Chen
+ */
+public class ShellSort {
+
+    private int[] sort(int[] arr){
+        int temp = 0;
+        for(int step = arr.length / 2; step > 0; step = arr.length / 2){
+            for (int i = step; i < arr.length; i++){
+                for (int j = i - step; j >= 0; j -= step){
+                    if(arr[j] > arr[j + step]){
+                        temp = arr[j];
+                        arr[j] = arr[j + step];
+                        arr[j + step] = temp;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+}
