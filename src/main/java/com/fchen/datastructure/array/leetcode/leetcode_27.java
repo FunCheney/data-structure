df0f9c492245d18426125cdec0f3e89285496365
Fchen <wanba@wanbadeMBP-2.lan>
2019-11-18 23:46:30 +0800

    leetCode 题解

---
+++ src/main/java/com/fchen/datastructure/array/leetcode/leetcode_27.java
@@ -0,0 +1,21 @@
+package com.fchen.datastructure.array.leetcode;
+
+/**
+ * @author: Fchen
+ * @date: 2019-11-18 08:05
+ * @desc: leetCode 第27题题解
+ */
+public class leetcode_27 {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        boolean flag = false;
+        for(int i = 0; i < nums.length - 1;i++){
+            if(nums[i] == val){
+                flag = true;
+                continue;
+            }
+            count++;
+        }
+        return flag ? count ++ : count;
+    }
+}
