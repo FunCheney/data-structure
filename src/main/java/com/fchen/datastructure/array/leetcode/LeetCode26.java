3d45e8e7b1e3cbf63f777d715979103dd64c509a
Fchen <wanba@wanbadeMacBook-Pro-2.local>
2019-11-19 08:39:02 +0800

    LeetCode 题解

--- src/main/java/com/fchen/datastructure/array/leetcode/LeetCode26.java
+++ src/main/java/com/fchen/datastructure/array/leetcode/LeetCode26.java
@@ -3,7 +3,16 @@ package com.fchen.datastructure.array.leetcode;
 /**
  * @author: Fchen
  * @date: 2019-11-18 23:51
- * @desc:
+ * @desc: Leet code 第26题
  */
 public class LeetCode26 {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++){
+            if(nums[j] != nums[i]){
+                nums[++ i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
 }
