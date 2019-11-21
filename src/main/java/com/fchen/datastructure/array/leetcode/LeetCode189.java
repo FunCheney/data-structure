c4500a7b6af10373ecf34e177e68365ee2657654
Fchen <fc_2012@163.com>
2019-11-21 23:05:02 +0800

    leetCode 题解

---
+++ src/main/java/com/fchen/datastructure/array/leetcode/LeetCode189.java
@@ -0,0 +1,12 @@
+package com.fchen.datastructure.array.leetcode;
+
+/**
+ * @author: Fchen
+ * @date: 2019-11-21 07:54
+ * @desc: 翻转数组
+ */
+public class LeetCode189 {
+    public void rotate(int[] nums, int k) {
+
+    }
+}
--- src/main/java/com/fchen/datastructure/array/leetcode/LeetCode80.java
+++ src/main/java/com/fchen/datastructure/array/leetcode/LeetCode80.java
@@ -7,24 +7,6 @@ package com.fchen.datastructure.array.leetcode;
  */
 public class LeetCode80 {
     public int removeDuplicates(int[] nums) {
-        //[0,0,1,1,1,1,2,3,3]
-        if(nums == null || nums.length == 0) {
-            return 0;
-        }
-        int i = 0;
-        int k = 0;
-        for(int j = 1; j < nums.length; j++){
-            int count = 0;
-            while (nums[i] == nums[j]){
-                count++;
-                if(count < 2) {
-                    i = j;
-                }else{
-                    i++;
-                }
-            }
-            nums[++ i] = nums[j];
-         }
-        return ++i;
+        return 0;
     }
 }
