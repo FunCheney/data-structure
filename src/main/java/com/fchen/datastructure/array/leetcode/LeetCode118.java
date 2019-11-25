525df1a95116b56b71695fad26d500fd68db800d
Fchen <fc_2012@163.com>
2019-11-25 22:57:51 +0800

    leetCode

---
+++ src/main/java/com/fchen/datastructure/array/leetcode/LeetCode118.java
@@ -0,0 +1,24 @@
+package com.fchen.datastructure.array.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Fchen
+ * @date: 2019-11-24 18:51
+ * @desc: leetCode 第118 题
+ */
+public class LeetCode118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        int count = 1;
+        while (count <= numRows){
+            List<Integer> data = new ArrayList<>();
+            for(int i = 1; i <= count; i++){
+
+            }
+            count++;
+        }
+        return result;
+    }
+}
